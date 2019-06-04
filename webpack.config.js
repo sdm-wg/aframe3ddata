78c71ecefa0c1f032b8c876f89a6c65b9012d538
KAWAMORITA Mitsuaki <kawa@hongo.wide.ad.jp>
2019-06-05 03:47:07 +0900

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  entry: {
+    app: './src/main.js',
+  },
+  output: {
+    filename: '[name].js',
+    path: `${__dirname}/assets`,
+    publicPath: '/assets/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      }
+    ]
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+      name: 'vendor'
+    }
+  },
+  devtool: 'source-map',
+  devServer: {
+    host: '0.0.0.0',
+    port: 9090,
+    disableHostCheck: true
+  }
+};
